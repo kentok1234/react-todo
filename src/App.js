4383a8dbd8031475733d2145b7ffc08a0f3324b6
Milda Aditia Putra <aditia.putra100@gmail.com>
2022-06-19 10:48:25 +0700

    fix(data): make todo with empty value

--- src/App.js
+++ src/App.js
@@ -6,15 +6,15 @@ import { useState } from 'react';
 function App() {
 
   const [contents, setCheckContent] = useState({
-    all: [{id: 1, check: false, title: 'Porta ac consectetur ac'}, {id: 2, check: false, title: 'Motorola'}, {id: 3, check: true, title: 'Ramayana'}],
-    active: [{id: 1, check: false, title: 'Porta ac consectetur ac'}, {id: 2, check: false, title: 'Motorola'}],
-    completed: [{id: 3, check: true, title: 'Ramayana'}]
+    all: [],
+    active: [],
+    completed: []
   })
 
   const addTodo = form => {
-    const newTodos = {...contents}
+    const newTodos = { ...contents }
     const newTodo = {
-      id: (contents.all.length > 0 ) ? Math.max(...contents.all.map(todo => todo.id)) + 1 : 1,
+      id: (contents.all.length > 0) ? Math.max(...contents.all.map(todo => todo.id)) + 1 : 1,
       check: false,
       title: form.todo
     }
@@ -24,22 +24,22 @@ function App() {
   }
 
   return (
-    <Row align='center' justify='center' style={{minHeight: '100vh', backgroundColor: 'ghostwhite'}}>
-      <Layout style={{maxWidth: '576px', margin: 'auto',padding: '2em', backgroundColor: 'white', boxShadow: '1em 1em 10px rgba(0, 0, 0, 0.1)', borderRadius: '1em'}}>
+    <Row align='center' justify='center' style={{ minHeight: '100vh', backgroundColor: 'ghostwhite' }}>
+      <Layout style={{ maxWidth: '576px', margin: 'auto', padding: '2em', backgroundColor: 'white', boxShadow: '1em 1em 10px rgba(0, 0, 0, 0.1)', borderRadius: '1em' }}>
         <Form onFinish={addTodo}>
-            <Row justify='center' gutter={[8, 8]}>
-              <Col span={16}>
-                <Form.Item name='todo' rules={[{required: true, message: "Can't add empty task"}]}>
-                  <Input placeholder='New task...' />
-                </Form.Item>
-              </Col>
-              <Col>
-                <Button type='primary' htmlType='submit'>ADD</Button>
-              </Col>
-            </Row>
+          <Row justify='center' gutter={[8, 8]}>
+            <Col span={16}>
+              <Form.Item name='todo' rules={[{ required: true, message: "Can't add empty task" }]}>
+                <Input placeholder='New task...' />
+              </Form.Item>
+            </Col>
+            <Col>
+              <Button type='primary' htmlType='submit'>ADD</Button>
+            </Col>
+          </Row>
         </Form>
 
-        <Todo dataTodos={contents} checkTodo={setCheckContent}/>
+        <Todo dataTodos={contents} checkTodo={setCheckContent} />
       </Layout>
     </Row>
   );
